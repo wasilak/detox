cf1cb5b70fd269228b646204061698c7b170287d
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2012-10-23 22:15:08 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Detox::Application.config.secret_token = '0189fe4fc18d857368fbbe14bf530c2d0aab4718cbc976dcd6e48668b63f21b37f7bfac03fa77291b694d5fd89451ac4c90ed5aa56f1cbc7d7ec30b1c63ed3f6'
