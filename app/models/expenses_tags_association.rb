9457f09608d3790691aa780bbbac3feb4f2d48b9
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2012-10-28 23:38:01 +0100

    added validation if tag was already assigned to expense

--- app/models/expenses_tags_association.rb
+++ app/models/expenses_tags_association.rb
@@ -3,6 +3,6 @@ class ExpensesTagsAssociation < ActiveRecord::Base
 	belongs_to :expense
   	attr_accessible :expense_id, :tag_id
 
-  	validates :tag_id, :presence => true
+  	validates :tag_id, :presence => true, :uniqueness => { :scope => :expense_id, :message => "this tag has already been assigned" }
 
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,8 @@ class Tag < ActiveRecord::Base
 	has_many :expenses_tags_association
   	attr_accessible :name
 
+	validates :name, :presence => true
+
   	def countExpenses
   		expenses_tags_association.find(:all).count
   	end
