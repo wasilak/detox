88f78488f1bab37283654f85715ad513a93fad94
Piotr Wasilewski <piotr.wasilewski@4prm.com>
2012-12-13 16:30:21 +0100

    Added Errbit config file

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,24 @@
-Airbrake.configure do |config|
-  config.api_key = 'be7224cb8cb6888d531728be00794841'
-  config.host    = 'bugs.wasil.org'
-  config.port    = 80
-  config.secure  = config.port == 443
-  # config.development_environments = []
+begin
+  ERRBIT_CONFIG = YAML.load_file("#{Rails.root}/config/errbit.yml")[Rails.env]
+rescue Errno::ENOENT
+  ERRBIT_CONFIG = false
+end
+
+print "* Loading Errbit config\t" + (ERRBIT_CONFIG ? "[ \033[32mSUCCESS\033[0m ]" : "[ \033[31mFAILURE\033[0m ]") + "\n"
+
+if ERRBIT_CONFIG
+  if ERRBIT_CONFIG['enabled']
+    print "* Errbit status\t[ \033[32mENABLED\033[0m ]\n"
+    Airbrake.configure do |config|
+      config.api_key = ERRBIT_CONFIG['api_key']
+      config.host    = ERRBIT_CONFIG['host']
+      config.port    = ERRBIT_CONFIG['port']
+      config.secure  = config.port == ERRBIT_CONFIG['secure']
+      if ERRBIT_CONFIG['development_environments']
+        config.development_environments = []
+      end
+    end
+  else
+    print "* Errbit status\t[ \033[31mDISABLED\033[0m ]\n"
+  end
 end
