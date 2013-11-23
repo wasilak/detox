0660dccdcbdc854d8708bff75c993fd96d7f6c90
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2013-11-23 19:54:05 +0100

    fixed date picker and alerts

--- app/assets/javascripts/bootstrap-datepicker.js
+++ app/assets/javascripts/bootstrap-datepicker.js
@@ -326,9 +326,10 @@
 
 		place: function(){
 						if(this.isInline) return;
-			var zIndex = parseInt(this.element.parents().filter(function() {
-							return $(this).css('z-index') != 'auto';
-						}).first().css('z-index'))+10;
+			// var zIndex = parseInt(this.element.parents().filter(function() {
+			// 				return $(this).css('z-index') != 'auto';
+			// 			}).first().css('z-index'))+10;
+      var zIndex = 99999;
 			var offset = this.component ? this.component.parent().offset() : this.element.offset();
 			var height = this.component ? this.component.outerHeight(true) : this.element.outerHeight(true);
 			this.picker.css({
