29cf4f17fb05f4881a1886e85cbee71b3b27ceb6
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2016-03-10 22:11:31 +0100

    Fixed locale support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.i18n.default_locale = APP_CONFIG['locale']
 end
