d80d401123deddea1253d0cb5d62346ecec42602
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2016-03-10 21:58:13 +0100

    Fixed problem with APP_CONFIG init

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,18 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+begin
+  APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+rescue Errno::ENOENT
+  APP_CONFIG = false
+end
+
+print "* Loading config\t" + (APP_CONFIG ? "[ \033[32mSUCCESS\033[0m ]" : "[ \033[31mFAILURE\033[0m ]") + "\n"
+
+unless APP_CONFIG
+  print "\033[36mNo config file found. Please copy config/config.yml.example to config/config.yml.\033[0m\n"
+  exit
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,17 +1,4 @@
 Rails.application.configure do
-  begin
-    APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
-  rescue Errno::ENOENT
-    APP_CONFIG = false
-  end
-
-  print "* Loading config\t" + (APP_CONFIG ? "[ \033[32mSUCCESS\033[0m ]" : "[ \033[31mFAILURE\033[0m ]") + "\n"
-
-  unless APP_CONFIG
-    print "\033[36mNo config file found. Please copy config/config.yml.example to config/config.yml.\033[0m\n"
-    exit
-  end
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
